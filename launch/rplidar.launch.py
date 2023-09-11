5d9a1fa018913467ca7d020d08a94007d3c2113e
zacharychuazetli <zachary.chua1234@gmail.com>
2023-09-11 09:13:24 +0800

    added lidar driver

---
+++ launch/rplidar.launch.py
@@ -0,0 +1,20 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    return LaunchDescription([
+
+        Node(
+            package='rplidar_ros',
+            executable='rplidar_composition',
+            output='screen',
+            parameters=[{
+                'serial_port': '/dev/serial/by-path/platform-70090000.xusb-usb-0:2.4:1.0-port0',
+                'frame_id': 'laser_frame',
+                'angle_compensate': True,
+                'scan_mode': 'Standard'
+            }]
+        )
+    ])
